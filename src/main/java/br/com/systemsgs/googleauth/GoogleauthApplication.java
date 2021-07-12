1566e22af0e270a2b3cb469a62bb56511c6a3bd0
GuilhermeJWT <guiromanno@gmail.com>
2021-07-12 13:34:30 -0300

    Setup Inicial Google OAuth2 - Spring Security

---
+++ src/main/java/br/com/systemsgs/googleauth/GoogleauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.googleauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleauthApplication.class, args);
+	}
+
+}
